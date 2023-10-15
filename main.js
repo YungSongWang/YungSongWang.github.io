fd4e474f6baf33e16be26d3ec637188a40692928
YungSongWang <137299819+YungSongWang@users.noreply.github.com>
2023-10-16 01:33:00 +1030

    Pmptimetable

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-import './style.scss'
+import './style.css'
 import './daycourse/export.js'
 import './daycourse/data.js'
+import './css/head.css';
+import './css/main-left.css';
+import './css/main-right.css';
 import { Mon } from './common.js'
 
